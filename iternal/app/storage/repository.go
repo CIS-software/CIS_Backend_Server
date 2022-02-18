34e0c676dfad519f0e7a6f1b62e35a7a0ffd4132
RoKZZ-55 <v.he2014@gmail.com>
2022-02-18 17:00:49 +0300

    Возможность удалить и изменить новость

--- iternal/app/storage/repository.go
+++ iternal/app/storage/repository.go
@@ -5,4 +5,6 @@ import "CIS_Backend_Server/iternal/app/model"
 type NewsRepository interface {
 	CreateNews(news *model.News) error
 	GetNews() ([]model.News, error)
+	UpdateNews(news *model.News) error
+	DeleteNews(news *model.News) error
 }
